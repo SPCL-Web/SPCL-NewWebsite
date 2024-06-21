6e79355263261686262d80ae13ef30679888912d
Sonia009 <134524881+Sonia09-coder@users.noreply.github.com>
2024-06-21 20:19:25 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -36,10 +36,9 @@
 
 // export default App;
 
-
 import "./App.css";
 import Header from "./components/common/header/Header";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route,  } from "react-router-dom";
 import About from "./components/about/About";
 import CourseHome from "./components/allcourses/CourseHome";
 import Team from "./components/team/Team";
@@ -50,8 +49,10 @@ import Footer from "./components/common/footer/Footer";
 import Home from "./components/home/Home";
 import Login from "./components/login/login";
 import Register from "./components/register/register";
-import ContactAdmin from "./components/contact/ContactAdmin";
-import RealBlog from './blogs/Blogs';
+// import ContactAdmin from "./components/contact/ContactAdmin"
+
+import Personalized from "./components/Personalized/Personalized";
+
 function App() {
   return (
     <>
@@ -60,7 +61,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/contact-admin" element={<ContactAdmin />} />
+          {/* <Route path="/contact-admin" element={<ContactAdmin />} /> */}
           <Route path="/courses" element={<CourseHome />} />
           <Route path="/team" element={<Team />} />
           <Route path="/pricing" element={<Pricing />} />
@@ -68,7 +69,8 @@ function App() {
           <Route path="/contact" element={<Contact />} />
           <Route path="/Login" element={<Login />} />
           <Route path="/Register" element={<Register />} />
-          <Route path="/blog" element={<RealBlog />} />
+          <Route path="/personalized" element={<Personalized />} />
+       
         </Routes>
         <Footer />
       </Router>
